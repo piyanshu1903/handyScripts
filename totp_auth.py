e39e0123ec469fd11c5f7de5c5827a2636385298
MOHITE PIYANSHU DATTATRAY <93767678+piyanshu1903@users.noreply.github.com>
2024-09-13 13:22:31 +0530

    Update and rename totp auth.md to totp_auth.py

--- totp_auth.py
+++ totp_auth.py
@@ -7,16 +7,17 @@ import qrcode
 secret=pyotp.random_base32()
 provisioning_uri = pyotp.totp.TOTP(secret).provisioning_uri(name='Piyanshu TOTP', issuer_name='PM.co')
 img = qrcode.make(provisioning_uri)
-img.save("./totp_qr.png") // it contains uri for authenticator apps
+img.save("./totp_qr.png") # It contains uri for authenticator apps
 
 
 totp = pyotp.TOTP(secret)
-print("Current OTP:", totp.now()) // Server end otp generation for a client
-totp.now() // stores current otp
-totp.verify(803283)// verifies clients otp
+print("Current OTP:", totp.now()) #Server end otp generation for a client
+totp.now() #stores current otp
+totp.verify(803283)   # verifies clients otp
 
 
-TOTP structure
+#TOTP structure
+'''
 class TOTP(
     s: str,
     digits: int = 6,
@@ -25,7 +26,7 @@ class TOTP(
     issuer: str | None = None,
     interval: int = 30
 )
-
+'''
 
 
 
